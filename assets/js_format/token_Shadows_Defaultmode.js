a06cd4178ce397438eb57de892abe7db381423c3
zeroheight <tokens-management@zeroheight.com>
2025-11-18 16:15:24 +0800

    Update LMS 5.0 Design System from zeroheight

---
+++ assets/js_format/token_Shadows_Defaultmode.js
@@ -0,0 +1,57 @@
+module.exports = {
+  "color": {
+    "Color": {
+      "lg": {
+        "value": "#000000",
+        "original": {
+          "value": "rgba(0, 0, 0, 0.1750)"
+        },
+        "name": "ColorColorLg",
+        "attributes": {
+          "category": "color",
+          "type": "Color",
+          "item": "lg"
+        },
+        "path": [
+          "color",
+          "Color",
+          "lg"
+        ]
+      },
+      "md (base)": {
+        "value": "#000000",
+        "original": {
+          "value": "rgba(0, 0, 0, 0.1500)"
+        },
+        "name": "ColorColorMdBase",
+        "attributes": {
+          "category": "color",
+          "type": "Color",
+          "item": "md (base)"
+        },
+        "path": [
+          "color",
+          "Color",
+          "md (base)"
+        ]
+      },
+      "sm": {
+        "value": "#000000",
+        "original": {
+          "value": "rgba(0, 0, 0, 0.0750)"
+        },
+        "name": "ColorColorSm",
+        "attributes": {
+          "category": "color",
+          "type": "Color",
+          "item": "sm"
+        },
+        "path": [
+          "color",
+          "Color",
+          "sm"
+        ]
+      }
+    }
+  }
+};
