174e3e073ce15b2a7ab63b3e60fe60efb03c1822
zeroheight <tokens-management@zeroheight.com>
2025-11-18 16:15:17 +0800

    Update LMS 5.0 Design System from zeroheight

---
+++ assets/flutter/token_Colors_Light.dart
@@ -0,0 +1,72 @@
+
+//
+// 
+//
+
+
+import 'dart:ui';
+
+class  {
+  ._();
+
+    static const colorBodyBackgroundDefault = Color(0xFFFFFFFF);
+    static const colorBodyBackgroundSecondary = Color(0xFFE9ECEF);
+    static const colorBodyBackgroundTertiary = Color(0xFFF8F9FA);
+    static const colorBodyTextDefault = Color(0xFF1D2125);
+    static const colorBodyTextEmphasis = Color(0xFF000000);
+    static const colorBodyTextSecondary = Color(0xBF1D2125);
+    static const colorBodyTextTertiary = Color(0x801D2125);
+    static const colorBorderDefault = Color(0xFFDEE2E6);
+    static const colorBorderTranslucent = Color(0x00000000);
+    static const colorBrandPrimaryMoodleBlack = Color(0xFF282828);
+    static const colorBrandPrimaryMoodleBlue = Color(0xFF194866);
+    static const colorBrandPrimaryMoodleOrange = Color(0xFFF98012);
+    static const colorBrandSecondaryBrightBlue = Color(0xFF69E0FF);
+    static const colorBrandSecondaryDarkGrey = Color(0xFF545454);
+    static const colorBrandSecondaryLightBlue = Color(0xFF65A1B3);
+    static const colorBrandSecondaryLightGrey = Color(0xFF8A8A8E);
+    static const colorBrandSecondaryLightPeach = Color(0xFFFFF8F1);
+    static const colorBrandSecondaryMediumPeach = Color(0xFFFFECDB);
+    static const colorBrandSecondaryPeach = Color(0xFFFEDEC3);
+    static const colorComponentActiveBackground = Color(0xFF0F6CBF);
+    static const colorComponentActiveText = Color(0xFFFFFFFF);
+    static const colorContrastContrastDark = Color(0xFF000000);
+    static const colorContrastContrastLight = Color(0xFFFFFFFF);
+    static const colorFocusRingDefault = Color(0xFF0F6CBF);
+    static const colorIconFaBorderColor = Color(0xFFEEEEEE);
+    static const colorIconFaInverse = Color(0xFFFFFFFF);
+    static const colorLinkDefault = Color(0xFF0F6CBF);
+    static const colorLinkHover = Color(0xFF0C5699);
+    static const colorThemeBackgroundSubtleDanger = Color(0xFFF4D6D2);
+    static const colorThemeBackgroundSubtleDark = Color(0xFFCED4DA);
+    static const colorThemeBackgroundSubtleInfo = Color(0xFFCCE6EA);
+    static const colorThemeBackgroundSubtleLight = Color(0xFFF8F9FA);
+    static const colorThemeBackgroundSubtlePrimary = Color(0xFFCFE2F2);
+    static const colorThemeBackgroundSubtleSecondary = Color(0xFFF8F9FA);
+    static const colorThemeBackgroundSubtleSuccess = Color(0xFFD7E4D6);
+    static const colorThemeBackgroundSubtleWarning = Color(0xFFFCEFDC);
+    static const colorThemeBorderSubtleDanger = Color(0xFFEAADA6);
+    static const colorThemeBorderSubtleDark = Color(0xFF8F959E);
+    static const colorThemeBorderSubtleInfo = Color(0xFFAECAAD);
+    static const colorThemeBorderSubtleLight = Color(0xFFE9ECEF);
+    static const colorThemeBorderSubtlePrimary = Color(0xFF9FC4E5);
+    static const colorThemeBorderSubtleSecondary = Color(0xFFE9ECEF);
+    static const colorThemeBorderSubtleSuccess = Color(0xFFAECAAD);
+    static const colorThemeBorderSubtleWarning = Color(0xFFF9DEB8);
+    static const colorThemeDanger = Color(0xFFCA3120);
+    static const colorThemeDark = Color(0xFF1D2125);
+    static const colorThemeInfo = Color(0xFF008196);
+    static const colorThemeLight = Color(0xFFF8F9FA);
+    static const colorThemePrimary = Color(0xFF0F6CBF);
+    static const colorThemeSecondary = Color(0xFFCED4DA);
+    static const colorThemeSuccess = Color(0xFF357A32);
+    static const colorThemeTextEmphasisDanger = Color(0xFF51140D);
+    static const colorThemeTextEmphasisDark = Color(0xFF495057);
+    static const colorThemeTextEmphasisInfo = Color(0xFF00343C);
+    static const colorThemeTextEmphasisLight = Color(0xFF495057);
+    static const colorThemeTextEmphasisPrimary = Color(0xFF062B4C);
+    static const colorThemeTextEmphasisSecondary = Color(0xFF343A40);
+    static const colorThemeTextEmphasisSuccess = Color(0xFF153114);
+    static const colorThemeTextEmphasisWarning = Color(0xFF60451F);
+    static const colorThemeWarning = Color(0xFFF0AD4E);
+}
