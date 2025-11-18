de48399756a0a15d857b8732a5b0a5bc9f6e1004
zeroheight <tokens-management@zeroheight.com>
2025-11-18 16:15:20 +0800

    Update LMS 5.0 Design System from zeroheight

---
+++ assets/flutter/token_Styletokens_Defaultmode.dart
@@ -0,0 +1,21 @@
+
+//
+// 
+//
+
+
+import 'dart:ui';
+
+class  {
+  ._();
+
+    static const colorBackgroundColorsPrimary = Color(0xFFFFFFFF);
+    static const colorBackgroundColorsSecondary = Color(0xFFE9ECEF);
+    static const colorBackgroundColorsTertiary = Color(0xFFF8F9FA);
+    static const colorTextColorsEmphasis = Color(0xFF000000);
+    static const colorTextColorsLink = Color(0xFF0F6CBF);
+    static const colorTextColorsLinkHover = Color(0xFF0C5699);
+    static const colorTextColorsPrimary = Color(0xFF1D2125);
+    static const colorTextColorsSecondary = Color(0xBF1D2125);
+    static const colorTextColorsTertiary = Color(0x801D2125);
+}
