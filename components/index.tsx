87646f2a72328c4a9a9a4392aa597efe21e76b3b
Mathew May <mathewm@hotmail.co.nz>
2025-11-20 15:50:15 +0800

    MDS-250 npm: Add Vite build & GHA

---
+++ components/index.tsx
@@ -0,0 +1,2 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+export * from './src'; // This will export all components from the 'src' folder
---
+++ components/src/button/index.tsx
@@ -0,0 +1,1 @@
+export * from './Button'; // This will export all components from the 'components' folder
