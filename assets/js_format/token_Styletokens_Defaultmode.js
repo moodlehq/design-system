ef04e5a0da7ccdbb34f29fd0b466eb772091f263
zeroheight <tokens-management@zeroheight.com>
2025-11-21 09:39:06 +0800

    Update LMS 5.0 Design System from zeroheight

---
+++ assets/js_format/token_Styletokens_Defaultmode.js
@@ -0,0 +1,161 @@
+module.exports = {
+  "color": {
+    "Background colors": {
+      "Primary": {
+        "value": "#ffffff",
+        "original": {
+          "value": "#ffffff"
+        },
+        "name": "ColorBackgroundColorsPrimary",
+        "attributes": {
+          "category": "color",
+          "type": "Background colors",
+          "item": "Primary"
+        },
+        "path": [
+          "color",
+          "Background colors",
+          "Primary"
+        ]
+      },
+      "Secondary": {
+        "value": "#e9ecef",
+        "original": {
+          "value": "#e9ecef"
+        },
+        "name": "ColorBackgroundColorsSecondary",
+        "attributes": {
+          "category": "color",
+          "type": "Background colors",
+          "item": "Secondary"
+        },
+        "path": [
+          "color",
+          "Background colors",
+          "Secondary"
+        ]
+      },
+      "Tertiary": {
+        "value": "#f8f9fa",
+        "original": {
+          "value": "#f8f9fa"
+        },
+        "name": "ColorBackgroundColorsTertiary",
+        "attributes": {
+          "category": "color",
+          "type": "Background colors",
+          "item": "Tertiary"
+        },
+        "path": [
+          "color",
+          "Background colors",
+          "Tertiary"
+        ]
+      }
+    },
+    "Text colors": {
+      "Emphasis": {
+        "value": "#000000",
+        "original": {
+          "value": "#000000"
+        },
+        "name": "ColorTextColorsEmphasis",
+        "attributes": {
+          "category": "color",
+          "type": "Text colors",
+          "item": "Emphasis"
+        },
+        "path": [
+          "color",
+          "Text colors",
+          "Emphasis"
+        ]
+      },
+      "Link": {
+        "value": "#0f6cbf",
+        "original": {
+          "value": "#0f6cbf"
+        },
+        "name": "ColorTextColorsLink",
+        "attributes": {
+          "category": "color",
+          "type": "Text colors",
+          "item": "Link"
+        },
+        "path": [
+          "color",
+          "Text colors",
+          "Link"
+        ]
+      },
+      "Link hover": {
+        "value": "#0c5699",
+        "original": {
+          "value": "#0c5699"
+        },
+        "name": "ColorTextColorsLinkHover",
+        "attributes": {
+          "category": "color",
+          "type": "Text colors",
+          "item": "Link hover"
+        },
+        "path": [
+          "color",
+          "Text colors",
+          "Link hover"
+        ]
+      },
+      "Primary": {
+        "value": "#1d2125",
+        "original": {
+          "value": "#1d2125"
+        },
+        "name": "ColorTextColorsPrimary",
+        "attributes": {
+          "category": "color",
+          "type": "Text colors",
+          "item": "Primary"
+        },
+        "path": [
+          "color",
+          "Text colors",
+          "Primary"
+        ]
+      },
+      "Secondary": {
+        "value": "#1d2125",
+        "original": {
+          "value": "rgba(29, 33, 37, 0.7500)"
+        },
+        "name": "ColorTextColorsSecondary",
+        "attributes": {
+          "category": "color",
+          "type": "Text colors",
+          "item": "Secondary"
+        },
+        "path": [
+          "color",
+          "Text colors",
+          "Secondary"
+        ]
+      },
+      "Tertiary": {
+        "value": "#1d2125",
+        "original": {
+          "value": "rgba(29, 33, 37, 0.5000)"
+        },
+        "name": "ColorTextColorsTertiary",
+        "attributes": {
+          "category": "color",
+          "type": "Text colors",
+          "item": "Tertiary"
+        },
+        "path": [
+          "color",
+          "Text colors",
+          "Tertiary"
+        ]
+      }
+    }
+  }
+};
