9fb4163ffc3dafb0c6110be9309324046b2dc1a5
zeroheight <tokens-management@zeroheight.com>
2025-11-21 09:38:58 +0800

    Update LMS 5.0 Design System from zeroheight

---
+++ assets/flutter/token_Typography_Defaultmode.dart
@@ -0,0 +1,19 @@
+
+//
+// 
+//
+
+
+import 'dart:ui';
+
+class  {
+  ._();
+
+    static const contentFontFamilyBase = "Roboto"; /* Default typeface for all UI text. Ensures global consistency. */
+    static const contentFontFamilyCode = "Menlo"; /* Typeface for code, preformatted text, or monospace contexts. */
+    static const contentFontWeightItalicBold = "Bold Italic";
+    static const contentFontWeightItalicLight = "Light Italic";
+    static const contentFontWeightItalicMedium = "Medium Italic";
+    static const contentFontWeightItalicNormalBase = "Italic";
+    static const contentFontWeightItalicSemibold = "Semi Bold Italic";
+}
