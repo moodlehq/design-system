fabb03951e6246b73de5788b3871998146614d7e
zeroheight <tokens-management@zeroheight.com>
2025-11-21 09:39:09 +0800

    Update LMS 5.0 Design System from zeroheight

---
+++ assets/js_format/token_Responsive_Mobile.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "content": {
+    "display": {
+      "value": "Fill",
+      "original": {
+        "value": "Fill"
+      },
+      "name": "ContentDisplay",
+      "attributes": {
+        "category": "content",
+        "type": "display"
+      },
+      "path": [
+        "content",
+        "display"
+      ]
+    }
+  }
+};
