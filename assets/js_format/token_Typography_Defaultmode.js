b94448b571c61b7ec8eebb273903339b37e67aaa
zeroheight <tokens-management@zeroheight.com>
2025-11-21 09:39:04 +0800

    Update LMS 5.0 Design System from zeroheight

---
+++ assets/js_format/token_Typography_Defaultmode.js
@@ -0,0 +1,143 @@
+module.exports = {
+  "content": {
+    "Font family": {
+      "base": {
+        "comment": "Default typeface for all UI text. Ensures global consistency.",
+        "value": "Roboto",
+        "original": {
+          "comment": "Default typeface for all UI text. Ensures global consistency.",
+          "value": "Roboto"
+        },
+        "name": "ContentFontFamilyBase",
+        "attributes": {
+          "category": "content",
+          "type": "Font family",
+          "item": "base"
+        },
+        "path": [
+          "content",
+          "Font family",
+          "base"
+        ]
+      },
+      "code": {
+        "comment": "Typeface for code, preformatted text, or monospace contexts.",
+        "value": "Menlo",
+        "original": {
+          "comment": "Typeface for code, preformatted text, or monospace contexts.",
+          "value": "Menlo"
+        },
+        "name": "ContentFontFamilyCode",
+        "attributes": {
+          "category": "content",
+          "type": "Font family",
+          "item": "code"
+        },
+        "path": [
+          "content",
+          "Font family",
+          "code"
+        ]
+      }
+    },
+    "Font weight": {
+      "Italic": {
+        "bold": {
+          "value": "Bold Italic",
+          "original": {
+            "value": "Bold Italic"
+          },
+          "name": "ContentFontWeightItalicBold",
+          "attributes": {
+            "category": "content",
+            "type": "Font weight",
+            "item": "Italic",
+            "subitem": "bold"
+          },
+          "path": [
+            "content",
+            "Font weight",
+            "Italic",
+            "bold"
+          ]
+        },
+        "light": {
+          "value": "Light Italic",
+          "original": {
+            "value": "Light Italic"
+          },
+          "name": "ContentFontWeightItalicLight",
+          "attributes": {
+            "category": "content",
+            "type": "Font weight",
+            "item": "Italic",
+            "subitem": "light"
+          },
+          "path": [
+            "content",
+            "Font weight",
+            "Italic",
+            "light"
+          ]
+        },
+        "medium": {
+          "value": "Medium Italic",
+          "original": {
+            "value": "Medium Italic"
+          },
+          "name": "ContentFontWeightItalicMedium",
+          "attributes": {
+            "category": "content",
+            "type": "Font weight",
+            "item": "Italic",
+            "subitem": "medium"
+          },
+          "path": [
+            "content",
+            "Font weight",
+            "Italic",
+            "medium"
+          ]
+        },
+        "normal (base)": {
+          "value": "Italic",
+          "original": {
+            "value": "Italic"
+          },
+          "name": "ContentFontWeightItalicNormalBase",
+          "attributes": {
+            "category": "content",
+            "type": "Font weight",
+            "item": "Italic",
+            "subitem": "normal (base)"
+          },
+          "path": [
+            "content",
+            "Font weight",
+            "Italic",
+            "normal (base)"
+          ]
+        },
+        "semibold": {
+          "value": "Semi Bold Italic",
+          "original": {
+            "value": "Semi Bold Italic"
+          },
+          "name": "ContentFontWeightItalicSemibold",
+          "attributes": {
+            "category": "content",
+            "type": "Font weight",
+            "item": "Italic",
+            "subitem": "semibold"
+          },
+          "path": [
+            "content",
+            "Font weight",
+            "Italic",
+            "semibold"
+          ]
+        }
+      }
+    }
+  }
+};
