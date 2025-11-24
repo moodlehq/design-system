42be9f261313e6a5812b66ceb6de8996f7fb237d
Andrew Gosali <andrew.gosali@moodle.com>
2025-11-24 15:25:36 +0800

    MDS-263 tokens: convert px to rem and append suffixes

--- scripts/tokens.ts
+++ scripts/tokens.ts
@@ -27,6 +27,11 @@ const tokenFiles = fs
  * Custom formatter: css/mds-variables
  * Generates CSS custom properties with 'mds-' prefix for primitives
  * and semantic tokens referencing the primitives (or referencing another semantic token).
+ *
+ * Why custom formatter?
+ * - Built-in formatter couldn't handle multi-layer semantic uppercased first letter condition.
+ * - Built-in formatter couldn't handle variables referencing to prefixed mds- primitives
+ * - Built-in formatter couldn't handle names with brackets i.e. (default) and (base)
  */
 StyleDictionary.registerFormat({
   name: 'css/mds-variables',
@@ -37,15 +42,23 @@ StyleDictionary.registerFormat({
     };
 
     const getTokenValue = (token: TransformedToken): string => {
+      /*
+        original value:
+        - primitive token -> the value before transformation
+        - semantic token -> the variable reference (not the resolved/transformed value)
+       */
       const originalValue = String(token.original.$value);
 
       // curly brackets -> it is a reference to another token
       const varMatch = originalValue.match(/^\{(.+)\}$/);
-      if (!varMatch) return originalValue;
+      if (!varMatch) return token.$value; // if primitive, return the transformed value instead of the original
       const varReference = varMatch[1];
 
-      // uppercase on the first letter -> reference to semantic token (no prefix)
-      // lowercase on the first letter -> reference to primitive token (mds- prefix).
+      /*
+        first letter:
+        - uppercase -> reference to semantic token (no prefix)
+        - lowercase -> reference to primitive token (mds- prefix).
+       */
       const prefix = /^[A-Z]/.test(varReference) ? '' : 'mds-';
       const cssVar = varReference
         .toLowerCase()
@@ -59,7 +72,7 @@ StyleDictionary.registerFormat({
       return `  --${getTokenName(token)}: ${getTokenValue(token)};`;
     });
 
-    return `${getFileHeader()}:root {\n${tokenVariables.join('\n')}\n}`;
+    return `${getFileHeader()}:root {\n${tokenVariables.join('\n')}\n}\n`;
   },
 });
 
@@ -72,9 +85,30 @@ StyleDictionary.registerFormat({
   format: ({ options }) => {
     return (
       getFileHeader() +
-      options.files.map((file: string) => `@import "./${file}";`).join('\n')
+      options.files.map((file: string) => `@import "./${file}";`).join('\n') +
+      '\n'
     );
   },
+});
+
+/**
+ * Custom transform: css/dimension-px-to-rem
+ * Transforms px values to rem for tokens related to dimensions.
+ *
+ * Why custom transform?
+ * - Built-in transform needs the type to be 'dimension', but our tokens solely use 'number' type.
+ * - The exported values in DTCG JSON from ZeroHeight would not have the units (ie. 1rem) for dimension-related tokens
+ */
+StyleDictionary.registerTransform({
+  name: 'css/dimension-px-to-rem',
+  type: 'value',
+  filter: (token) => {
+    return (
+      token.$type === 'number' &&
+      /rem|font-size|line-height|border-width/i.test(token.name)
+    );
+  },
+  transform: (token) => `${token.$value / 16}rem`,
 });
 
 /**
@@ -85,6 +119,7 @@ new StyleDictionary({
   platforms: {
     css: {
       transformGroup: 'css',
+      transforms: ['css/dimension-px-to-rem'],
       buildPath: 'tokens/css',
       files: [
         ...tokenFiles.map((fileName) => ({
@@ -106,11 +141,6 @@ new StyleDictionary({
           },
         },
       ],
-    },
-  },
-  hooks: {
-    transformGroups: {
-      css: ['size/pxToRem'],
     },
   },
 }).buildAllPlatforms();
