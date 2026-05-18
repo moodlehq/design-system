a711898812ad217eb8d9fafd6e98494b5730d0c8
Andrew Nicols <andrew@nicols.co.uk>
2026-05-18 14:42:34 +0800

    feat(Tokens): Build CSS variables at run-time from SCSS
    Moodle allows tokens to be updated by an administrator at run-time.
    This patch exposes the SCSS variables as CSS variables which are
    resolved at compile time as an optional supplement to the
    scss/_index.scss.

--- scripts/tokens.ts
+++ scripts/tokens.ts
@@ -72,6 +72,28 @@ StyleDictionary.registerFormat({
   },
 });
 
+StyleDictionary.registerFormat({
+  name: 'scss/css-variables-map',
+  format: function ({ dictionary }) {
+    // Generate the :root block referencing the SCSS variables
+    const rootLines = dictionary.allTokens
+      .map((token) => {
+        // SCSS interpolation syntax is #{$variable}
+        return `  --${token.name}: #{$${token.name}};`;
+      })
+      .join('\n');
+
+    return (
+      `
+        // ${getFileHeaderContent()}
+
+        :root {
+        ${rootLines}
+        }`.replace(/^ {8}/gm, '') + '\n'
+    );
+  },
+});
+
 /**
  * Custom transform: dimension-px-to-rem
  * Transforms px values to rem for tokens related to dimensions.
@@ -160,6 +182,15 @@ new StyleDictionary({
         {
           destination: '_index.scss',
           format: 'scss/aggregator',
+          options: {
+            files: tokenFiles.map((fileName) =>
+              convertJsonFileName(fileName, '_', 'scss'),
+            ),
+          },
+        },
+        {
+          destination: '_index_css_vars.scss',
+          format: 'scss/css-variables-map',
           options: {
             files: tokenFiles.map((fileName) =>
               convertJsonFileName(fileName, '_', 'scss'),
